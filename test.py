7ab597d8a723b49b84817a9ebe72ea0741fd09a2
Bernard <bkusi@mesika.org>
2024-09-17 15:22:31 +0000

    Learning about constructer in classes
    Signed-off-by: Bernard <bkusi@mesika.org>

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("Hello world")
+class Person():
+    def __init__(self,name,age,job):
+        self.name = name
+        self.age = age
+        self.job = job
+        return
+        
+
+    def describeperson(self):
+        return "Your name is " + self.name + " ," + str(self.age) + " years of age"
+
+bernard = Person("Bernard",25,"Engineer")
+
+print(bernard.describeperson())
